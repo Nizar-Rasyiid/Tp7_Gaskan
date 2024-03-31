501b058b46ecf9a1d62d28f4cb2e075c12a9015a
Marjauza Naswansyah <marjauzann@gmail.com>
2024-04-01 05:47:20 +0700

    naswan_class

--- tp7/Program.cs
+++ tp7/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using tp7;
+
+namespace tp7_DataMhs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa1302220037_Nizar dataMahasiswa = new DataMahasiswa1302220009_Naswan();
+            dataMahasiswa.ReadJSON();
+
+            KuliahMahasiswa_1302220037_Nizar kuliah = new KuliahMahasiswa_1302220009_Naswan();
+            kuliah.ReadJson();
+        }
+    }
+}
